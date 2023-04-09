e06a9f159aaaa8e1198d3c90b3d3271851862da5
Nathan Grasso <nathangrasso@yahoo.com>
2023-04-09 14:45:51 -0600

    Initial Commit

---
+++ Vigor/VigorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VigorApp.swift
+//  Vigor
+//
+//  Created by Nathan Grasso on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct VigorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
