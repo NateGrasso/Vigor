58fcd5ec35568127a8adc2e5bacce4bd5df88e64
Nathan Grasso <nathangrasso@yahoo.com>
2023-04-16 20:48:20 -0600

    Initial login screen UI

--- Vigor/ContentView.swift
+++ Vigor/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = ""
+    @State private var wrongPassword = ""
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Learning Github in Xcode!")
+        NavigationStack{
+            ZStack{
+                Color.teal
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white.opacity(0.20))
+                VStack{
+                    Text("VIGOR")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        
+                    Spacer()
+                    
+                    Text("[Logo here]")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.red, lineWidth: 2))
+                    
+                    SecureField("password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.red, lineWidth: 2))
+                    
+                    Button("Login"){
+                        //Authenticate user
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.teal)
+                    .cornerRadius(10)
+                    
+                    Text("Dont have an account?")
+                        .font(.subheadline)
+                    
+                    Button("Sign Up"){
+                        //direct to sign up form
+                    }
+                    .bold()
+                    .foregroundColor(.black)
+                    .background(Color.teal.opacity(0.05))
+                    .frame(width: 100, height: 10)
+                    Spacer()
+                }
+            }
         }
-        .padding()
+        .navigationBarHidden(true)
+    }
+    func authenticateUser(username: String, password: String){
+        
     }
 }
 
@@ -24,3 +86,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
